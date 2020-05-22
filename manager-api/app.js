95c77e10874f1c39dc4ed85a79e171b1a50ad936
jcaguirre-CL <jcaguirrecl@gmail.com>
2020-05-22 10:25:00 -0400

    CORS Authorization

--- manager-api/app.js
+++ manager-api/app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'ejs');
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
   res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
